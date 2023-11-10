7f0f3c2b7838ed584597d919990bfac9ac891a39
Yixuan Xia <yixuanxi@andrew.cmu.edu>
2023-11-10 14:28:34 -0500

    updated video capturing for main and connect4 class

--- connect4.py
+++ connect4.py
@@ -1,19 +1,36 @@
+class chess:
+    state = 0
+    def __init__(self,state):
+        self.state=state
+
+    def get_state(self):
+        return self.state
+
+    def set_state(self,state):
+        if state == 0 or state == 1 or state ==2:
+            self.state = state
+        else:
+            print("state should be 0, 1, 2. which 0 for no chess, 1 for black, 2 for white")
 
 class connect4:
     width = 0
     height = 0
     board = []
+
     def __init__(self,boardwidth,boardheight):
-        if( boardheight >= 4 and boardwidth>=4 ):
+        if( boardheight >= 4 and boardwidth >= 4 ):
             self.width = boardwidth
             self.height = boardheight
-            self.board = chess[boardheight][boardwidth]
-            for i in range(0,boardheight):
-                for j in range(0,boardwidth):
-                    chess[i][j] = chess(0)
+            self.board = [[chess(0) for i in range(boardheight)] for j in range(boardwidth)]
         else:
             print("size of the board should be greater than 4 x 4")
-     
+    
+    def get_width(self):
+        return self.width
+    
+    def get_height(self):
+        return self.height
+    
     def put_chess(self, state, col):
         pos = 0
         while pos < self.height:
@@ -36,6 +53,7 @@ class connect4:
                     continue
                 else:
                     break
+
         for i in range(1,4):
             if col+i > 0 and col+i < self.width:
                 if chess[pos][col+i].get_state == state:
@@ -44,6 +62,7 @@ class connect4:
                     continue
                 else:
                     break
+
         for i in range(1,4):
             if col-i > 0 and col-i < self.width:
                 if chess[pos][col-i].get_state == state:
@@ -52,7 +71,7 @@ class connect4:
                     continue
                 else:
                     break
-
+                
         for i in range(1,4):
             if col-i > 0 and col-i < self.width and pos+i > 0 and pos+i < self.height:
                 if chess[pos+i][col-i].get_state == state:
@@ -93,18 +112,5 @@ class connect4:
         return [False,state]
     
 
-class chess:
-    state = 0
-    
-    def __init__(self,state):
-        self.state=state
 
-    def get_state(self):
-        return self.state
-
-    def set_state(self,state):
-        if state == 0 or state == 1 or state ==2:
-            self.state = state
-        else:
-            print("state should be 0, 1, 2. which 0 for no chess, 1 for black, 2 for white")
     
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import connect4
 import cv2
 
-connect4game = connect4(10,7)
+cameraId = 1 # give a specific camera id connected to the computer
+connect4game = connect4.connect4(10,7) #testing class connection
+
+# start video stream
+capture = cv2.VideoCapture(cameraId) 
+cv2.namedWindow('camera', cv2.WINDOW_NORMAL)  # open a window to show
+while capture.isOpened():
+    ret, frame = capture.read()
+    if ret:
+        if frame is not None:
+            cv2.imshow('camera', frame)  # show the frame
+            cv2.waitKey(1)
+    
+# clear up
+capture.release()
+cv2.destroyAllWindows()
+
